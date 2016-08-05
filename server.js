5768be4f76da14f5f225935dc1870075d8f9b072
emrahkilic <emrahkilic2330@gmail.com>
2016-08-05 23:04:32 +0300

    ad

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 var port = Number(process.env.PORT || 3000);
-var server = app.listen(port);
-var io = require('socket.io').listen(server);
+var io = require('socket.io')(http);
 
 
 
