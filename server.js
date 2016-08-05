f2b555c7fbff03d822df05247f8ec181bfd722e9
emrahkilic <emrahkilic2330@gmail.com>
2016-08-05 22:38:08 +0300

    ad

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-server.listen(process.env.PORT || 5000);
+var express = require('express');
+var app = express();
+var port = Number(process.env.PORT || 5000);
+var server = app.listen(port);
+var io = require('socket.io').listen(server);
 
 var kullanicilar = {};
 var sohbetler = {};
